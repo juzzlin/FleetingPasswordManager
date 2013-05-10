e20cbf7fc276f521f59cc9b4cd84d51dcb539956
Jussi Lind <jussi.lind@iki.fi>
2013-05-10 19:19:29 +0300

    2.8

--- scripts/archive.sh
+++ scripts/archive.sh
@@ -1,1 +1,4 @@
-git archive --format=tar --prefix=fleetingpm-2.7/ HEAD | gzip > fleetingpm-2.7.tar.gz
+VERSION=$(git log | grep -m 1 "^\s*[0-9]\.[0-9]\(\.[0-9]\)*" | awk '{print $1}')
+git archive --format=tar --prefix=fleetingpm-$VERSION/ HEAD | gzip > fleetingpm-$VERSION.tar.gz
+git archive --format=zip --prefix=fleetingpm-$VERSION/ HEAD > fleetingpm-$VERSION.zip
+

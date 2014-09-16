ea264762bdec015c1a576ac9a63fc5f1bb0fd7af
Jussi Lind <jussi.lind@iki.fi>
2014-09-16 17:31:51 +0300

    2.9.0

--- scripts/updatePPA.sh
+++ scripts/updatePPA.sh
@@ -2,10 +2,10 @@ VERSION=${FLEETINGPM_RELEASE_VERSION?"is not set."}
 DEBIAN_VERSION=$VERSION-1
 NAME=fleetingpm
 rm -rf *${VERSION}
-cp ../sourceforge-git/$NAME/$NAME-$VERSION.tar.gz .
-mv $NAME-$VERSION.tar.gz $NAME_$VERSION.orig.tar.gz 
-tar xzvf $NAME_$VERSION.orig.tar.gz
+cp ../sourceforge-git/$NAME-code/$NAME-$VERSION.tar.gz .
+mv $NAME-$VERSION.tar.gz ${NAME}_${VERSION}.orig.tar.gz
+tar xzvf ${NAME}_${VERSION}.orig.tar.gz
 cd $NAME-$VERSION
 cp -rv packaging/debian .
-debuild -S -sa && cd .. && dput ppa:jussi-lind/ppa "dustrac_${DEBIAN_VERSION}_source.changes"
+debuild -S -sa && cd .. && dput ppa:jussi-lind/ppa ${NAME}_${DEBIAN_VERSION}_source.changes
 
